45411fbb6de53f0a1dc7c321d66fa8e138e8ea28
lesktimo <l3skt1m@gmail.com>
2017-03-31 15:42:48 +0300

    manual maven done

---
+++ newGradle/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import ohtu.Multiplier;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);		
+		System.out.print("Who are you? ");
+		String nimi = s.nextLine();
+		System.out.println("Now, I am become " + nimi 
+					+", destroyer of the Worlds!");
+		Multiplier nolla = new Multiplier(0);
+		System.out.print("Gimme a number! ");
+		int luku = s.nextInt();
+		System.out.println("Given number multiplied by zero is " + 
+					nolla.multipliedBy(luku) + "!");
+	}
+}
